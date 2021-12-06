47f47bc46f8725410246bca9e7a17778dcd3ceba
Lucas Garcia <garcia@et.esiea.fr>
2021-12-06 21:29:46 +0100

    commit PART 6

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -3,6 +3,7 @@ package fr.lernejo.logger;
 public class LoggerFactory {
     public static Logger getLogger( String name ) {
     	//MODIF PART 5
+    	//MODIF PART 6
         FileLogger fileLogger = new FileLogger( "./runLogs.log" );
         FilteredLogger filteredLogger = new FilteredLogger( fileLogger, message -> message.contains( "simulation" ) );
         CompositeLogger compositeLogger = new CompositeLogger( new ConsoleLogger(), filteredLogger );
